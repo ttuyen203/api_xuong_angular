e6b45f81575d82bec3023d31f3fa759b4b94a6f2
ttuyen <totuyencv@gmail.com>
2024-06-05 23:39:14 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server"); // nhập thư viện json-server
+const auth = require("json-server-auth"); // nhập thư viện json-server-auth
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; // chọn cổng từ đây như 8080, 3001
 
+// Gắn kết cơ sở dữ liệu của router với ứng dụng
+server.db = router.db
+
+// Bạn phải áp dụng middleware auth trước router
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server đang chạy trên cổng ${port}`);
+});
